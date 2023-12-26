307ce792a81de8500afdc3dc29b9b96399084f28
richarlisooon <147280214+richarlisooon@users.noreply.github.com>
2023-12-26 21:02:58 +0300

    Add files via upload

--- lab2.c
+++ lab2.c
@@ -4,19 +4,42 @@ int main(){
 	double a,b,c,D,x1,x2,rx,ix;
 	scanf("%lf %lf %lf",&a,&b,&c);
 	D=b*b-4*a*c;
-	if (D==0){
-		x1=-b/2*a;
-		printf("уравнение иммет 1 корень x=%lf\n",x1);
+	if (a==0 && b==0 && c==0){
+		printf("Бесконечно много решений\n");
 	}
-	else if (D>0){
-		x1=(-b+sqrt(D))/(2*a);
-		x2=(-b-sqrt(D))/(2*a);
-		printf("корни уравнений x1=%lf x2=%lf\n",x1,x2);
+	else if (a==0 && b==0){
+		printf("Нет корней\n");
 	}
-	else if (D<0) {
-		rx=-b/(2*a);
-		ix=sqrt(-D)/(2*a);
-		printf("корни уравнения x1=%lf + %lfi x2=%lf - %lfi\n",rx,ix,rx,ix);
+	else if (a==0 && c==0){
+                printf("Уравнение имеет 1 корень x1=0\n");
+        }
+	else if (b==0 && c==0){
+		printf("Уравнение имеет 1 корень x1=0\n");
+	}
+	else if (a==0){
+		printf("Уравнение имеет 1 корень равный %lf\n",b/c);
+	}
+	else if (b==0){
+		printf("Уравнение имеет 2 корня x1=%lf,x2=%lf\n",c,-c);
+	}
+	else if (c==0){
+		printf("Уравнение имеет 2 корня x1=0 x2=%lf\n",-b/a);
+	}
+	else {
+		if (D==0){
+			x1=-b/2*a;
+			printf("уравнение иммет 1 корень x=%lf\n",x1);
+		}
+		else if (D>0){
+			x1=(-b+sqrt(D))/(2*a);
+			x2=(-b-sqrt(D))/(2*a);
+			printf("корни уравнений x1=%lf x2=%lf\n",x1,x2);
+		}
+		else if (D<0) {
+			rx=-b/(2*a);
+			ix=sqrt(-D)/(2*a);
+			printf("корни уравнения x1=%lf + %lfi x2=%lf - %lfi\n",rx,ix,rx,ix);
+		}
 	}
 	return 0;
 }
